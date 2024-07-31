1c6348700e9d43545c816263dd3eb52e3e6a9401
quanglinh12340 <149453502+quanglinh12340@users.noreply.github.com>
2024-07-31 22:02:21 +0700

    render ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,15 @@
 import React from "react";
-
+import { Outlet } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  );
 };
 
 export default App;
