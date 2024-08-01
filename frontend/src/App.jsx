80622ba46c089fae7865b2044a10cab9f143b3e3
quanglinh12340 <149453502+quanglinh12340@users.noreply.github.com>
2024-08-01 23:13:40 +0700

    change footer position

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,9 @@ const App = () => {
   return (
     <>
       <Header />
-      <Outlet />
+      <main className="min-h-[calc(100vh-120px)]">
+        <Outlet />
+      </main>{" "}
       <Footer />
     </>
   );
