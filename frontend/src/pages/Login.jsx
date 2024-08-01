f9ebfc1f37d8c78c544b45632d2adb20dd85e39e
quanglinh12340 <149453502+quanglinh12340@users.noreply.github.com>
2024-08-01 23:12:21 +0700

    Get input data

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,26 +1,47 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
-
 import loginIcons from "@/assets/signin.gif";
+
 const Login = () => {
   const [showPassword, setShowPassword] = useState(false);
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+    setData((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
 
   return (
     <section id="login">
       <div className=" container mx-auto p-4">
-        <div className="bg-white p-2 py-5 w-full max-w-md mx-auto">
+        <div className="bg-white  p-5 w-full max-w-sm mx-auto">
           <div className="w-20 h-20 mx-auto">
             <img src={loginIcons} alt="login icons" />
           </div>
-          <form>
+          <form className="pt-6 flex flex-col gap-2" onSubmit={handleSubmit}>
             <div className=" grid">
               <label>Email:</label>
               <div className="bg-slate-100 p-2">
                 <input
+                  value={data.email}
                   type="email"
-                  placeholder="Enter email"
+                  name="email"
+                  placeholder="Enter your email"
                   className="w-full h-full outline-none bg-transparent"
+                  onChange={handleOnChange}
                 />
               </div>
             </div>
@@ -28,9 +49,12 @@ const Login = () => {
               <label>Password:</label>
               <div className="bg-slate-100 p-2 flex">
                 <input
+                  value={data.password}
                   type={showPassword ? "text" : "password"}
-                  placeholder="Enter password"
+                  name="password"
+                  placeholder="Enter your password"
                   className="w-full h-full outline-none bg-transparent"
+                  onChange={handleOnChange}
                 />
                 <div
                   className="cursor-pointer text-lg"
@@ -46,10 +70,19 @@ const Login = () => {
                 Forgot Password
               </Link>
             </div>
-            <button className="bg-red-600 text-white px-6 py-2 w-full max-w-[150px] rounded-full hover:scale-110 transition-all mx-auto block mt-6">
+            <button className="bg-red-600 hover:bg-red-700 text-white px-6 py-2 w-full max-w-[150px] rounded-full hover:scale-110 transition-all mx-auto block mt-6">
               Login
             </button>
           </form>
+          <p className="my-5">
+            Don't have account ?
+            <Link
+              to={"/sign-up"}
+              className="text-red-600 hover:text-red-700 hover:underline"
+            >
+              Sign up
+            </Link>
+          </p>
         </div>
       </div>
     </section>
