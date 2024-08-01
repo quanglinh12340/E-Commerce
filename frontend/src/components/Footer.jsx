0bba05cb85b1e79145c4d2ab998a6c0fbdd07e7b
quanglinh12340 <149453502+quanglinh12340@users.noreply.github.com>
2024-08-01 23:12:53 +0700

    Footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="bg-slate-200">
+      <div className="container mx-auto p-4">
+        <p className="text-center font-bold ">Dynamic Coding with Amit</p>
+      </div>{" "}
+    </footer>
+  );
 };
 
 export default Footer;
