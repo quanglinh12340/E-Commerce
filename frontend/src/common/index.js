17bc44cb77e31da57f9d0d7c20b707c709cd3033
quanglinh12340 <149453502+quanglinh12340@users.noreply.github.com>
2024-08-20 09:45:42 +0700

    Sign In and Sign Up url

---
+++ frontend/src/common/index.js
@@ -0,0 +1,14 @@
+const backendDomain = "http://localhost:8080"
+
+const SummaryApi = {
+    signUp: {
+        url: `${backendDomain}/api/signup`,
+        method: "POST"
+    },
+    signIn: {
+        url: `${backendDomain}/api/signin`,
+        method: "POST"
+    }
+}
+
+export default SummaryApi
