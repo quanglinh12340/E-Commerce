95eb8156c19645376ab8492eb2e6db0ae9bacbf7
quanglinh12340 <149453502+quanglinh12340@users.noreply.github.com>
2024-08-23 23:11:34 +0700

    use context get detail user

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,46 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+
+import SummaryApi from "./common";
+import Context from "./context";
+import { useDispatch } from "react-redux";
+import { setUserDetails } from "./store/userSlice";
 const App = () => {
+  const dispatch = useDispatch();
+  const fetchUserDetails = async () => {
+    const dataResponse = await fetch(SummaryApi.current_user.url, {
+      method: SummaryApi.current_user.method,
+      credentials: "include",
+    });
+
+    const dataApi = await dataResponse.json();
+
+    if (dataApi.success) {
+      dispatch(setUserDetails(dataApi.data));
+    }
+  };
+
+  useEffect(() => {
+    fetchUserDetails();
+  }, []);
   return (
     <>
-      <ToastContainer />
-      <Header />
-      <main className="min-h-[calc(100vh-120px)]">
-        <Outlet />
-      </main>{" "}
-      <Footer />
+      <Context.Provider
+        value={{
+          fetchUserDetails,
+        }}
+      >
+        <ToastContainer />
+        <Header />
+        <main className="min-h-[calc(100vh-120px)]">
+          <Outlet />
+        </main>{" "}
+        <Footer />
+      </Context.Provider>
     </>
   );
 };
