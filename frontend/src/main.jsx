74d1b9d9f87513283115c874773209e919b5841b
quanglinh12340 <149453502+quanglinh12340@users.noreply.github.com>
2024-08-23 23:10:50 +0700

    use reducer

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
+import { Provider } from "react-redux";
 import router from "./router";
-
+import { store } from "./store/store.js";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>{" "}
   </React.StrictMode>
 );
