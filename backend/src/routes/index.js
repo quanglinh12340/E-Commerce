eaa697daece29b3c66c7b41862f0d2dfd96ead5f
quanglinh12340 <149453502+quanglinh12340@users.noreply.github.com>
2024-08-23 23:04:32 +0700

    user details route

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import userSignUpController from "../controllers/userSignUp.controller.js";
 import userSignInController from "../controllers/userSignIn.controller .js";
+import authToken from "../middlewares/authToken.middleware.js";
+import userDetailsController from "../controllers/userDetails.controller.js";
+import { userLogout } from "../controllers/userLogout.controller.js";
 
 const RootRouter = Router()
 
 RootRouter.post('/signup', userSignUpController)
 RootRouter.post('/signin', userSignInController)
+RootRouter.get('/user-details', authToken, userDetailsController)
 
 export { RootRouter }
