908255a6dc09f2781fccc449e030985bd713b80a
quanglinh12340 <149453502+quanglinh12340@users.noreply.github.com>
2024-09-01 00:03:48 +0700

    render HorizontalCardProduct component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,38 @@
 import React from "react";
 import BannerProduct from "@/components/BannerProduct";
 import CategoryList from "@/components/CategoryList";
+import HorizontalCardProduct from "@/components/HorizontalCardProduct";
 
 const Home = () => {
   return (
     <div>
       <CategoryList />
       <BannerProduct />
+      <HorizontalCardProduct category={"airpodes"} heading={"Top's Airpodes"} />
+      <HorizontalCardProduct
+        category={"earphones"}
+        heading={"Popular's Watches"}
+      />
+      <HorizontalCardProduct category={"airpodes"} heading={"Top's Airpodes"} />
+      <HorizontalCardProduct
+        category={"earphones"}
+        heading={"Popular's Watches"}
+      />
+      <HorizontalCardProduct category={"airpodes"} heading={"Top's Airpodes"} />
+      <HorizontalCardProduct
+        category={"earphones"}
+        heading={"Popular's Watches"}
+      />
+      <HorizontalCardProduct category={"airpodes"} heading={"Top's Airpodes"} />
+      <HorizontalCardProduct
+        category={"earphones"}
+        heading={"Popular's Watches"}
+      />
+      <HorizontalCardProduct category={"airpodes"} heading={"Top's Airpodes"} />
+      <HorizontalCardProduct
+        category={"earphones"}
+        heading={"Popular's Watches"}
+      />
     </div>
   );
 };
