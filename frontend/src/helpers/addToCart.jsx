aad94972c7bf2284ce4e62fb684eeaaa72ac676d
quanglinh12340 <149453502+quanglinh12340@users.noreply.github.com>
2024-09-06 23:53:49 +0700

    file ađ to cart to add product quantity by id

--- frontend/src/helpers/addToCart.jsx
+++ frontend/src/helpers/addToCart.jsx
@@ -1,6 +1,29 @@
-const addToCart = (e, id) => {
+import SummaryApi from "@/common";
+import { toast } from "react-toastify";
+
+const addToCart = async (e, id) => {
   e?.stopPropagation();
   e?.preventDefault();
+
+  const fetchResponse = await fetch(SummaryApi.addToCart_product.url, {
+    method: SummaryApi.addToCart_product.method,
+    credentials: "include",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify({
+      productId: id,
+    }),
+  });
+
+  const dataResponse = await fetchResponse.json();
+
+  if (dataResponse.success) {
+    toast.success(dataResponse.message);
+  }
+  if (dataResponse.error) {
+    toast.error(dataResponse.message);
+  }
 };
 
 export default addToCart;
