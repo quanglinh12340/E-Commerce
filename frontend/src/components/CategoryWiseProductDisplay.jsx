3cdfcbcd3db6e52336f42b5d5953b7696b6f20a3
quanglinh12340 <149453502+quanglinh12340@users.noreply.github.com>
2024-09-06 19:17:14 +0700

    Category Wise Product Display component

---
+++ frontend/src/components/CategoryWiseProductDisplay.jsx
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+
+import { FaCartPlus } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+import fetchCategoryWiseProduct from "@/helpers/fetchCategoryWiseProduct";
+import displayINRCurrency from "@/helpers/displayCurrency";
+import "@/assets/css/HorizontalCardProduct.css";
+import addToCart from "@/helpers/addToCart";
+import renderStars from "@/helpers/renderStars";
+const CategoryWiseProductDisplay = ({ category, heading }) => {
+  const [data, setData] = useState([]);
+
+  const fetchData = async () => {
+    const categoryProduct = await fetchCategoryWiseProduct(category);
+    setData(categoryProduct?.data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      <h2 className="text-2xl font-semibold py-4">{heading}</h2>
+      <div className="grid grid-cols-[repeat(auto-fit,minmax(300px,320px))] justify-between md:gap-6 overflow-x-scroll scrollbar-none transition-all ">
+        {data.map((product, index) => {
+          return (
+            <Link to={"product/" + product?._id} key={index} id="product1">
+              <div className="pro-container">
+                <div className="pro">
+                  <img src={product.productImage[0]} alt="" />
+                  <div className="des">
+                    <h5>{product?.productName}</h5>
+                    <span>{product?.category}</span>
+                    <div className="star">{renderStars()}</div>
+                    <h4>{displayINRCurrency(product?.price)}</h4>
+                  </div>
+                  <button className="cart">
+                    <FaCartPlus
+                      className="cart-plus"
+                      onClick={(e) => addToCart(e, product?._id)}
+                    />
+                  </button>
+                </div>
+              </div>
+            </Link>
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+CategoryWiseProductDisplay.propTypes = {
+  category: PropTypes.string,
+  heading: PropTypes.string,
+};
+
+export default CategoryWiseProductDisplay;
