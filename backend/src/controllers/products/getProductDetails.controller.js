01c9261e068aab377b869e94c9ebc0eecf55117f
quanglinh12340 <149453502+quanglinh12340@users.noreply.github.com>
2024-09-06 16:27:21 +0700

    get product details logic

---
+++ backend/src/controllers/products/getProductDetails.controller.js
@@ -0,0 +1,26 @@
+import ProductModel from "../../models/product.model.js"
+
+async function getProductDetailsController(req, res) {
+    try {
+
+        const { productId } = req.body
+
+        const product = await ProductModel.findById(productId)
+
+        res.json({
+            message: "Ok",
+            data: product,
+            success: true,
+            error: false
+        })
+
+    } catch (error) {
+        res.status(400).json({
+            message: err.message || err,
+            error: true,
+            success: false
+        })
+    }
+}
+
+export default getProductDetailsController
--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,16 +1,20 @@
 import { Router } from "express";
+
+import authToken from "../middlewares/authToken.middleware.js";
+
 import userSignUpController from "../controllers/users/userSignUp.controller.js";
 import userSignInController from "../controllers/users/userSignIn.controller .js";
-import authToken from "../middlewares/authToken.middleware.js";
-import userDetailsController from "../controllers/users/userDetails.controller.js";
 import { userLogout } from "../controllers/users/userLogout.controller.js";
+import userDetailsController from "../controllers/users/userDetails.controller.js";
 import allUserController from "../controllers/users/allUser.controller.js";
 import updateUserController from "../controllers/users/updateUser.controller.js";
+
 import uploadProductController from "../controllers/products/uploadProduct.controller.js";
 import getProductController from "../controllers/products/getProduct.controller.js";
 import updateProductController from "../controllers/products/updateProduct.controller.js";
 import getCategoryProductController from "../controllers/products/getCategoryProductOne.controller.js";
 import getCategoryWiseProductController from "../controllers/products/getCategoryWiseProduct.controller.js";
+import getProductDetailsController from "../controllers/products/getProductDetails.controller.js";
 
 const RootRouter = Router()
 
@@ -29,4 +33,6 @@ RootRouter.get('/get-product', getProductController)
 RootRouter.post('/update-product', authToken, updateProductController)
 RootRouter.get('/get-categoryProduct', getCategoryProductController)
 RootRouter.post('/category-product', getCategoryWiseProductController)
+RootRouter.post('/product-details', getProductDetailsController)
+
 export { RootRouter }
